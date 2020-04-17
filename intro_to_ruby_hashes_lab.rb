d3362123bae59aa481f1ffdb08ba59782f63e431
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-17 04:35:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,12 +29,12 @@ def monopoly_with_third_tier
   base_hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
+      :rent_in_dollars => {
+        :one_piece_owned => 25
+      },
       :names => {}
     }
   }
-  base_hash[:rent_in_dollars] = 25
-  base_hash
 end
 
 def monopoly_with_fourth_tier
